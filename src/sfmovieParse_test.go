c81246d7a55a6ec1f981ebff91091a5e008b52f8
Liqing Chen <liqing.chen@shopee.com>
2020-08-15 23:05:55 +0800

    sfmovieParse

---
+++ src/sfmovieParse_test.go
@@ -0,0 +1,16 @@
+package src
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+)
+
+func Test_parseStr(t *testing.T){
+	//colIdx, _ := strconv.ParseInt("03", 16, 64)
+	seatStatus, _ := strconv.Atoi("5")
+	//fmt.Printf("%d",colIdx)
+	fmt.Printf("%d",seatStatus)
+
+}
+
